3fb403d411c923036f3cc5bfdd84149470c4c939
Jang-oi <jjh53241@gmail.com>
2022-06-27 14:37:46 +0900

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useRef} from "react";
+import axios from "axios";
+// MUI
+import {Box, Button, Container, TextField} from "@mui/material";
+import {customAlert} from "./utils/commonUtil";
+// 컴포넌트
+import Board from "./component/Board";
+import Loading from "./component/Loding";
+import {useNavigate} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [url, setUrl] = useState('');
+    const [tableData, setTableData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const urlInputRef = useRef(null);
+    let navigate = useNavigate();
+
+    const serverUrl = 'http://localhost:3000';
+
+    /**
+     * 확인버튼 클릭 시 이벤트
+     * @param e
+     */
+    const onSubmitHandler = (e) => {
+        if (!url.toLowerCase().includes('smartstore.naver.com')) {
+            customAlert({
+                icon : 'error',
+                title: 'Oops...',
+                text : '입력하신 URL 확인 부탁드립니다.'
+            }).then(() => {
+                setTimeout(() => {
+                    urlInputRef.current.focus();
+                }, 300);
+            });
+        } else {
+            getHtml().then(response => {
+                setTableData(response.data);
+                setLoading(false);
+            }).catch(() => {
+                setLoading(false);
+                customAlert({
+                    icon : 'error',
+                    title: 'Oops...',
+                    text : '다시 시도 부탁드립니다.'
+                }).then(() => {
+                    navigate('/');
+                });
+            });
+        }
+        e.preventDefault();
+    }
+
+
+    /**
+     * 크롤링 이후 데이터 가져오는 api 호출
+     * @returns {Promise<AxiosResponse<any>>}
+     */
+    const getHtml = async () => {
+        setLoading(true);
+        return await axios.post(`${serverUrl}/getReview`, {url: url}, {timeout: 10000});
+    };
+
+    /**
+     * URL 입력 시 이벤트
+     * @param e
+     */
+    const onUrlHandler = (e) => {
+        setUrl(e.currentTarget.value);
+    }
+
+    return (
+        <Container>
+            {loading && <Loading/>}
+            <Box
+                component="form"
+                onSubmit={onSubmitHandler}
+                sx={{
+                    marginTop    : 8,
+                    display      : 'flex',
+                    flexDirection: 'column',
+                    alignItems   : 'center',
+                }}
+            >
+                <TextField variant="standard" autoFocus fullWidth label="URL을 입력해주세요." value={url} inputRef={urlInputRef}
+                           onChange={onUrlHandler}/>
+                <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}} disabled={!url}>확인</Button>
+                <Board tableData={tableData}/>
+            </Box>
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/component/Board.jsx
@@ -0,0 +1,32 @@
+// MUI
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+
+const Board = ({tableData}) => {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{minWidth: 650}} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>순위</TableCell>
+                        <TableCell>옵션명</TableCell>
+                        <TableCell>리뷰 개수</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {tableData.map((data, index) => (
+                        <TableRow
+                            key={index}
+                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                        >
+                            <TableCell>{index+1}</TableCell>
+                            <TableCell>{data.productOptionContent}</TableCell>
+                            <TableCell>{data.cnt}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default Board;
