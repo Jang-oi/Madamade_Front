ed13f29b0ef703de7b7ff2ef4786d6bf358b3947
Jang-oi <jjh53241@gmail.com>
2023-01-23 22:34:44 +0900

    v1

--- src/apis/smartBenchmark.js
+++ src/apis/smartBenchmark.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/mada/';
+const BASE_URL = 'http://43.201.2.159:8080/mada'
 
 export default axios.create({
     baseURL: BASE_URL,
