d18f635144f8b098e6e69af67f15ea1d54496256
jeongho <jeongho@unipost.co.kr>
2023-01-25 15:25:08 +0900

    V1

--- src/component/Submit.jsx
+++ src/component/Submit.jsx
@@ -1,5 +1,7 @@
-import { Box, Button, TextField, Typography } from '@mui/material';
-import { Fragment, useRef } from 'react';
+import { Box, Divider, IconButton, InputBase, Paper, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ClearIcon from '@mui/icons-material/Clear';
+import { Fragment } from 'react';
 import { urlValidate } from '../utils/validateUtil';
 import axios from '../apis/smartBenchmark';
 import { useAxios } from '../customHooks/useAxios';
@@ -8,7 +10,6 @@ import { useFetchDataDispatch } from '../contexts/fetchDataContext';
 
 const Submit = ({ pageUrl, menuName }) => {
 
-    const urlInputRef = useRef(null);
     const { axiosFetch } = useAxios();
 
     const urlState = useUrlState();
@@ -17,11 +18,6 @@ const Submit = ({ pageUrl, menuName }) => {
     const { url } = urlState;
     const fetchDataDispatch = useFetchDataDispatch();
 
-    const errorCallBack = () => {
-        setTimeout(() => {
-            urlInputRef.current.focus();
-        }, 300);
-    };
 
     /**
      * 확인버튼 클릭 시 이벤트
@@ -29,7 +25,7 @@ const Submit = ({ pageUrl, menuName }) => {
      */
     const onSubmitHandler = async (e) => {
         e.preventDefault();
-        await urlValidate(url, errorCallBack);
+        await urlValidate(url);
         const fetchObj = {};
 
         switch (pageUrl) {
@@ -69,13 +65,19 @@ const Submit = ({ pageUrl, menuName }) => {
         urlDispatch({ type: 'SET_URL', url: e.currentTarget.value });
     };
 
+    /**
+     * URL 초기화
+     */
+    const onUrlRemoveHandler = () => {
+        urlDispatch({ type: 'SET_URL', url: '' });
+    };
+
     return (
         <Fragment>
             <Typography variant='h5' component='div' style={{ textAlign: 'center', marginTop: '20px' }}>
                 {menuName}
             </Typography>
             <Box
-                component='form'
                 onSubmit={onSubmitHandler}
                 sx={{
                     display      : 'flex',
@@ -83,18 +85,26 @@ const Submit = ({ pageUrl, menuName }) => {
                     alignItems   : 'center',
                 }}
             >
-                <TextField
-                    variant='standard'
-                    autoFocus
-                    fullWidth
-                    label='URL을 입력해주세요.'
-                    value={url}
-                    inputRef={urlInputRef}
-                    onChange={onUrlHandler}
-                />
-                <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }} disabled={!url}>
-                    확인
-                </Button>
+                <Paper
+                    component='form'
+                    sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '80%', marginBottom: '15px' }}
+                >
+                    <InputBase
+                        sx={{ ml: 1, flex: 1 }}
+                        placeholder='URL을 입력해주세요.'
+                        inputProps={{ 'aria-label': 'search google maps' }}
+                        value={url}
+                        onChange={onUrlHandler}
+                        autoFocus
+                    />
+                    <IconButton type='button' sx={{ p: '10px' }} aria-label='search' onClick={onSubmitHandler}>
+                        <SearchIcon />
+                    </IconButton>
+                    <Divider sx={{ height: 28, m: 0.5 }} orientation='vertical' />
+                    <IconButton color='primary' sx={{ p: '10px' }} aria-label='directions' onClick={onUrlRemoveHandler}>
+                        <ClearIcon />
+                    </IconButton>
+                </Paper>
             </Box>
         </Fragment>
     );
--- src/utils/validateUtil.js
+++ src/utils/validateUtil.js
@@ -11,7 +11,10 @@ export const urlValidate = async (url, errorCallBackFn) => {
     if (url.toLowerCase().includes('https://smartstore.naver.com')) bool = false;
     if (url.toLowerCase().includes('https://brand.naver.com')) bool = false;
 
-    if (bool) errorAlert('입력하신 URL 확인 부탁드립니다.', errorCallBackFn);
+    if (bool) {
+        if (!errorCallBackFn) errorCallBackFn = () => {};
+        errorAlert('입력하신 URL 확인 부탁드립니다.', errorCallBackFn);
+    }
 };
 
 /**
@@ -20,7 +23,8 @@ export const urlValidate = async (url, errorCallBackFn) => {
  * @param errorCallBackFn
  */
 export const errorAlert = (text, errorCallBackFn) => {
-    if (!errorCallBackFn) errorCallBackFn = () => {};
+    if (!errorCallBackFn) errorCallBackFn = () => {
+    };
     customAlert({ text, isError: true }, errorCallBackFn);
     throw new Error(text);
 };
